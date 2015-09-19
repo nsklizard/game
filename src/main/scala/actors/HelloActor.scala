f77cd5c558d1e2dc0c8ff88e6d680228ef1c41c8
Dmitry O Kim <nskcodernsk@gmail.com>
2015-09-20 05:33:12 +0600

    ignore

---
+++ src/main/scala/actors/HelloActor.scala
@@ -0,0 +1,28 @@
+package actors
+
+import akka.actor.{Props, ActorSystem, Actor}
+import helloworld.MyServiceActor
+
+/**
+ * Created by lizard on 20.09.2015.
+ */
+object HelloActor extends App{
+  val system = ActorSystem("mysys")
+  val cnt = system.actorOf(Props[CCCounter], "counter")
+
+  val r = cnt!SumDigits(1, 2)
+
+
+//  val c: CCCounter = new CCCounter
+////  val r:Result = c!SumDigits(1, 2)
+//  println(r)
+}
+
+case class SumDigits(a:Int, b:Int)
+case class Result(c:Int)
+
+class CCCounter extends Actor{
+  override def receive: Receive = {
+    case SumDigits(a,b) => println(a+b)
+  }
+}
---
+++ src/main/scala/helloworld/Hello.scala
@@ -0,0 +1,53 @@
+package helloworld
+
+import scala.concurrent.{Future, Await}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import slick.backend.DatabasePublisher
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import slick.driver.PostgresDriver.api._
+
+import scala.util.{Failure, Success}
+
+/**
+ * Created by lizard on 14.09.2015.
+ */
+object Hello extends App {
+
+  // we need an ActorSystem to host our application in
+  implicit val system = ActorSystem("on-spray-can")
+
+  // create and start our service actor
+  val service = system.actorOf(Props[MyServiceActor], "demo-service")
+
+  implicit val timeout = Timeout(5.seconds)
+  // start a new HTTP server on port 8080 with our service actor as the handler
+  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+
+
+
+
+  val connectionUrl = "jdbc:postgresql://localhost/scala?user=postgres&password=root"
+  val db = Database.forURL(connectionUrl, driver = "org.postgresql.Driver")
+
+  try {
+    val users: TableQuery[Users] = TableQuery[Users]
+
+    println("Users:")
+    db.run (users.result) onComplete {
+      case Failure( t) => // logging
+      case Success(res) => println(res)
+    }
+
+//    val add = DBIO.seq(
+//      users +=(0, "Test2", 20L)
+//    )
+//    val f = db.run(add.transactionally)
+
+  } finally db.close
+}
